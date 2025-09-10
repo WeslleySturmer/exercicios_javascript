65a3dc8df3939a199ba7fbd885cc5c8d08aebe23
WeslleySturmann <weslley.w.sturmer@gmail.com>
2025-09-10 10:13:27 -0300

    Aula 11 - Estruturas condicionais simples e compostas

---
+++ aula11/condicao_simples.js
@@ -0,0 +1,8 @@
+let velocidade = 85
+
+console.log(`A velocidade do seu carro é de ${velocidade} km/h`)
+
+//condição simples
+if (velocidade > 80){
+    console.log(`Sua velocidade ultrapassou o limite de 80km/h, multado!`)
+}
