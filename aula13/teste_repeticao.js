2ed4e4b9f59f0c5df281d95fe29d74eec2cffc08
WeslleySturmann <weslley.w.sturmer@gmail.com>
2025-09-11 16:07:13 -0300

    Aula 13 - Repetições pt1 - While e do while

---
+++ aula13/teste_repeticao.js
@@ -0,0 +1,16 @@
+let i = 1
+
+console.log('Usando while')
+
+while (i <= 5){
+    console.log(i)
+    i++
+}
+
+console.log('Agora com DO')
+
+i = 1
+do {
+    console.log(i)
+    i++
+} while (i <= 5)
