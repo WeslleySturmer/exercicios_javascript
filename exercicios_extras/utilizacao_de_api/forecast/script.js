daf182f565f380e76bc8b77d14cf9b0039510a09
WeslleySturmann <weslley.w.sturmer@gmail.com>
2025-09-20 15:08:31 -0300

    Ajustes finais no forecast, adição de mensagem 'buscando dados' e emojis como variáveis

--- exercicios_extras/utilizacao_de_api/forecast/script.js
+++ exercicios_extras/utilizacao_de_api/forecast/script.js
@@ -34,6 +34,10 @@ async function buscaCidade() {
 
     let retornoApiCidade = await chamaApiCidade(cidade)
 
+    divResposta.classList.remove('hidden')
+    divResposta.style.padding = '20px'
+    divResposta.innerHTML = '<p>Buscando dados...</p>'
+
     if (retornoApiCidade[0] == null) {
         divResposta.classList.add('hidden')
         divResposta.style.padding = '0px'
@@ -41,7 +45,15 @@ async function buscaCidade() {
 
         divNotFound.classList.remove('hidden')
         divNotFound.style.padding = '20px'
-        divNotFound.innerHTML = `<img src="" alt="notfound_img"> <p>Não foi possível encontrar essa cidade :/</p>`
+
+        let imagem_not_found = document.createElement('img')
+        imagem_not_found.src = "https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png"
+        imagem_not_found.alt = "notfound_image"
+
+        let texto_not_found = document.createElement('p')
+        texto_not_found.innerHTML = 'Não foi possível encontrar essa cidade :/'
+
+        divNotFound.append(imagem_not_found, texto_not_found)
     } else {
         let retornoApiTempo = await chamaApiTempo(retornoApiCidade[0].lat, retornoApiCidade[0].lon)
 
@@ -51,17 +63,28 @@ async function buscaCidade() {
 
         divResposta.innerHTML = ''
 
+        const EMOJIS = {
+                sun: "&#9728",
+                cloudy: "&#127781",
+                rain: "&#127783",
+                snow: "&#127784",
+                default: "&#127785"
+        }
+
+        let emoji_resp = document.createElement('p')
+        emoji_resp.id = 'emoji_tempo'
+
         switch (retornoApiTempo.current_weather.weathercode){
             case 0:
             case 1:
             case 2:
-                divResposta.innerHTML = `<p id="emoji_tempo">&#9728</p>`
+                emoji_resp.innerHTML = EMOJIS.sun
                 break;
             
             case 3:
             case 45:
             case 48:
-                divResposta.innerHTML = `<p id="emoji_tempo">&#127781</p>`
+                emoji_resp.innerHTML = EMOJIS.cloudy
                 break;
 
             case 51:
@@ -77,7 +100,7 @@ async function buscaCidade() {
             case 57:
             case 66: 
             case 67:
-                divResposta.innerHTML = `<p id="emoji_tempo">&#127783</p>`
+                emoji_resp.innerHTML = EMOJIS.rain
                 break;
 
             case 77:
@@ -86,16 +109,21 @@ async function buscaCidade() {
             case 71:
             case 73:
             case 75:
-                divResposta.innerHTML = `<p id="emoji_tempo">&#127784</p>`
+                emoji_resp.innerHTML = EMOJIS.snow
                 break;
 
             default:
-                divResposta.innerHTML = `<p id="emoji_tempo">&#127785</p>`
+                emoji_resp.innerHTML = EMOJIS.default
                 break;
         }
+
+        let temperatura = document.createElement('p')
+        temperatura.innerHTML = `<p>Temperatura: ${retornoApiTempo.current_weather.temperature} ${retornoApiTempo.current_weather_units.temperature}</p>` 
             
-        divResposta.innerHTML += `<p>Temperatura: ${retornoApiTempo.current_weather.temperature} ${retornoApiTempo.current_weather_units.temperature}</p>
-        <p>Velocidade do vento: ${retornoApiTempo.current_weather.windspeed} ${retornoApiTempo.current_weather_units.windspeed}</p>`
+        let velocidade_vento = document.createElement('p')
+        velocidade_vento.innerHTML = `<p>Velocidade do vento: ${retornoApiTempo.current_weather.windspeed} ${retornoApiTempo.current_weather_units.windspeed}</p>`
+        
+        divResposta.append(emoji_resp, temperatura, velocidade_vento)
 
         divResposta.classList.remove('hidden')
         divResposta.style.padding = '20px'
